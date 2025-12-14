d6daf00dc1b85e8ef79e2bfa5d3395c8076555fb
Rad099 <209367335+WesleyB12@users.noreply.github.com>
2025-12-14 14:24:12 -0600

    number comparing program

--- test.py
+++ test.py
@@ -1,11 +1,16 @@
-x = 1
-y = 2
-
-if (x > y):
-    print (x, " is greater than", y)
-    if (x < y):
-        print (x, " is less than", y)
+def comparisons(x, y):
+    if (x > y):
+        print (x, " is greater than", y)
+        if (x < y):
+            print (x, " is less than", y)
+        else:
+            print (x, " is not less than", y)
     else:
-        print (x, " is not less than", y)
-else:
-    print (x, " is not greater than", y)
+        print (x, " is not greater than", y)
+
+
+
+while(True):
+    x = int (input("Enter a number for x: "))
+    y = int (input("Enter a number for y: "))
+    comparisons(x,y)
