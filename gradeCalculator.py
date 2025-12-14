204bb65e3a5afeee280df21af7b55e184f407960
Rad099 <209367335+WesleyB12@users.noreply.github.com>
2025-12-14 14:46:29 -0600

    Update gradeCalculator.py

--- gradeCalculator.py
+++ gradeCalculator.py
@@ -1,12 +1,19 @@
-score = int(input("Enter your grade: "))
+import math
 
-if (score >= 90):
-    print("Your grade is an 'A'")
-elif (score >= 80):
-    print("Your grade is an 'B'")
-elif (score >= 70):
-    print("Your grade is an 'C'")
-elif (score >= 60):
-    print("Your grade is an 'D'")
-else:
-    print("Your grade is an 'F'")
+def gradeCalculator (score):
+    if (score >= 90):
+        print("Your grade is an 'A'")
+    elif (score >= 80):
+        print("Your grade is an 'B'")
+    elif (score >= 70):
+        print("Your grade is an 'C'")
+    elif (score >= 60):
+        print("Your grade is an 'D'")
+    else:
+        print("Your grade is an 'F'")
+        
+
+while(True):
+    score = float(input("Enter your grade: "))
+    score = math.ceil(score)
+    gradeCalculator(score)
